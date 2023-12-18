ec37c4a17933b23e44d6b69c679f54592f196d8f
Mashhurbek Mashrabov <mashkhurbek.mashrabov@gmail.com>
2023-12-18 22:46:10 +0500

    updated utils.py

--- app/apps/bot/utils.py
+++ app/apps/bot/utils.py
@@ -1,9 +1,11 @@
 import logging
 import traceback
+from typing import Union
 
+import requests
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-from bot.constants import EXCEPTION_CHANNEL_ID, CallbackData, BotUserSteps
+from bot.constants import EXCEPTION_CHANNEL_ID, CallbackData, BotUserSteps, BOT_TOKEN
 from bot.loader import bot
 from bot.strings import messages
 
@@ -31,3 +33,82 @@ def send_exception(error_text, step, user=None):
     except Exception as e:
         logger.error(traceback.format_exc())
         logger.error(e)
+
+
+def generate_telegram_file_url(file_id: str) -> str:
+    """
+    Returns a direct link to a Telegram file hosted on the Telegram servers.
+
+    Args:
+        file_id (str): The unique identifier of the file.
+
+    Returns:
+        str: The direct link to the Telegram file.
+    """
+    file_info = bot.get_file(file_id)
+    return f"https://api.telegram.org/file/bot{BOT_TOKEN}/{file_info.file_path}"
+
+
+def price_formatter(price: Union[int, float]) -> str:
+    """
+    Formats a price value into a human-readable string with proper commas and decimal point handling.
+
+    Args:
+        price (Union[int, float]): The price value to be formatted.
+
+    Returns:
+        str: The formatted price string.
+    """
+
+    try:
+        formatted_price = f"{price:,.2f}"
+    except TypeError:
+        return str(price)
+
+    # Trim trailing '.00' and replace commas with spaces
+    formatted_price = formatted_price.rstrip(".0").replace(",", " ")
+
+    return formatted_price
+
+
+def get_client_ip(request):
+    """
+    Retrieves the client's IP address from the request object, accounting for potential proxies.
+
+    Args:
+        request (HttpRequest): The Django request object.
+
+    Returns:
+        str: The client's IP address as a string.
+    """
+    forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR', None)
+    remote_addr = request.META.get('REMOTE_ADDR', None)
+    if forwarded_for:
+        # Split and take the first address if forwarded
+        return forwarded_for.split(",")[0].strip()
+    else:
+        return remote_addr
+
+
+def get_ip_location(ip_address):
+    """
+    Retrieves location information for an IP address using an external API.
+
+    Args:
+        ip_address (str): The client's IP address.
+
+    Returns:
+        dict or None: Dictionary containing location data if successful, None otherwise.
+
+    Example:
+        location = get_ip_location(get_client_ip(request))
+        if location:
+            print(f"Country: {location['country']}")
+            print(f"City: {location['city']}")
+    """
+
+    response = requests.get(f"http://ip-api.com/json/{ip_address}")
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
