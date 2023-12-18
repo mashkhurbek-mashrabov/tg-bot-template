4ce483600be01ee0a8c8f68286f80bb66a5b7fac
Mashhurbek Mashrabov <mashkhurbek.mashrabov@gmail.com>
2023-12-18 23:03:09 +0500

    redis was set up for caching

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -92,6 +92,17 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "IGNORE_EXCEPTIONS": True,
+        },
+    }
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
